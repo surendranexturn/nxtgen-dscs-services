0db4cb3d2deef58b32beb21d14ce02c925781938
surendranexturn <surendra.t@nexturn.com>
2023-10-17 11:28:51 +0530

    Pick Queries

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Login = require("./login");
 const JsonConverison = require("./common");
 const Inventory = require("./inventory");
+const Pick = require("./pick");
 
-module.exports = { Login, JsonConverison, Inventory };
+module.exports = { Login, JsonConverison, Inventory, Pick };
