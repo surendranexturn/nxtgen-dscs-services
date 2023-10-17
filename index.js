be59eae6aa08543b5044617dd5e7026a442e792b
surendranexturn <surendra.t@nexturn.com>
2023-10-17 12:08:21 +0530

    Pack

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ const Login = require("./login");
 const JsonConverison = require("./common");
 const Inventory = require("./inventory");
 const Pick = require("./pick");
+const Pack = require("./pack");
 
-module.exports = { Login, JsonConverison, Inventory, Pick };
+module.exports = { Login, JsonConverison, Inventory, Pick, Pack };
